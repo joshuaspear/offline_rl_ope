fixed wis calculation and added laplace smoothing

src/offline_rl_ope/OPEEstimators/utils.py
@@ -1,45 +1,92 @@
+from abc import ABCMeta, abstractmethod
+from typing import Any
 import torch
 
 
-def wis_norm_weights(traj_is_weights:torch.Tensor, is_msk:torch.Tensor
-                     )->torch.Tensor:
-    """Normalised propensity weights according to 'weighted importance sampling'
+class WeightNorm(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def __call__(self, traj_is_weights:torch.Tensor, is_msk:torch.Tensor
+                 ) -> torch.Tensor:
+        pass
+    
+class WISNormWeights(WeightNorm):
+    
+    def __init__(self, smooth_eps:float=0.0, *args, **kwargs) -> None:
+        self.smooth_eps = smooth_eps
+    
+    def calc_norm(self, traj_is_weights:torch.Tensor, is_msk:torch.Tensor
+                  ) -> torch.Tensor:
+        """Calculates the denominator for weighted importance sampling i.e.
+        w_{t} = 1/n sum_{i=1}^{n} p_{1:t}. Note, if traj_is_weights represent
+        vanilla IS samples then this will be w_{t} = 1/n sum_{i=1}^{n} p_{1:H}
+        for all samples. is_msk.sum(axis=0, keepdim=True) is taken as the 
+        denominator since it is required to take the average over valid time t 
+        importance ratios. This may differ for different episodes.
+        ref: http://proceedings.mlr.press/v48/jiang16.pdf
+        smooth_eps prevents nan values occuring in instances where there exists
+        valid time t importance ratios however, these are all 0. This should
+        be set as small as possible. 
 
-    Args:
-        traj_is_weights (torch.Tensor): (# trajectories, max(traj_length)) 
-            Tensor. traj_is_weights[i,j] defines the jth timestep propensity 
-            weight for the ith trajectory
-        is_msk (torch.Tensor): (# trajectories, max(traj_length)) binary 
-            Tensor. weight_msk[i,j] defines whether the jth timestep of the
-            ith trajectory was observed
+        Args:
+            traj_is_weights (torch.Tensor): (# trajectories, max(traj_length)) 
+                Tensor. traj_is_weights[i,j] defines the jth timestep propensity 
+                weight for the ith trajectory
+            is_msk (torch.Tensor): (# trajectories, max(traj_length)) binary 
+                Tensor. weight_msk[i,j] defines whether the jth timestep of the
+                ith trajectory was observed
 
-    Returns:
-        torch.Tensor: Tensor of dimension (# trajectories, max(traj_length)) 
-        with normalised weights
-    """
-    denom:torch.Tensor = traj_is_weights.sum(axis=0, keepdim=True)
-    res = traj_is_weights/denom
-    return res
+        Returns:
+            torch.Tensor: Tensor of dimension (# trajectories, 1) defining the 
+            normalisation value for each timestep
+        """
+        denom:torch.Tensor = traj_is_weights.sum(axis=0, keepdim=True)
+        denom = (denom+self.smooth_eps)/(
+            is_msk.sum(axis=0, keepdim=True)+self.smooth_eps)
+        return denom
+    
+    def __call__(self, traj_is_weights:torch.Tensor, is_msk:torch.Tensor
+                 ) -> torch.Tensor:
+        """Normalised propensity weights according to 
+        'weighted importance sampling'. 
 
+        Args:
+            traj_is_weights (torch.Tensor): (# trajectories, max(traj_length)) 
+                Tensor. traj_is_weights[i,j] defines the jth timestep propensity 
+                weight for the ith trajectory
+            is_msk (torch.Tensor): (# trajectories, max(traj_length)) binary 
+                Tensor. weight_msk[i,j] defines whether the jth timestep of the
+                ith trajectory was observed
 
-def norm_weights_pass(traj_is_weights:torch.Tensor, is_msk:torch.Tensor
-                      )->torch.Tensor:
-    """Helper function
+        Returns:
+            torch.Tensor: Tensor of dimension (# trajectories, max(traj_length)) 
+            with normalised weights
+        """
+        denom = self.calc_norm(traj_is_weights=traj_is_weights, is_msk=is_msk)
+        res = traj_is_weights/(denom+self.smooth_eps)
+        return res
+    
+class NormWeightsPass(WeightNorm):
+    
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+    
+    def __call__(self, traj_is_weights:torch.Tensor, is_msk:torch.Tensor
+                 )->torch.Tensor:
+        """Helper function
 
-    Args:
-        traj_is_weights (torch.Tensor): (# trajectories, max(traj_length)) 
-            Tensor. traj_is_weights[i,j] defines the jth timestep propensity 
-            weight for the ith trajectory
-        is_msk (torch.Tensor): (# trajectories, max(traj_length)) binary 
-            Tensor. weight_msk[i,j] defines whether the jth timestep of the
-            ith trajectory was observed
+        Args:
+            traj_is_weights (torch.Tensor): (# trajectories, max(traj_length)) 
+                Tensor. traj_is_weights[i,j] defines the jth timestep propensity 
+                weight for the ith trajectory
+            is_msk (torch.Tensor): (# trajectories, max(traj_length)) binary 
+                Tensor. weight_msk[i,j] defines whether the jth timestep of the
+                ith trajectory was observed
 
-    Returns:
-        torch.Tensor: Identical tensor to traj_is_weights
-    """
-    return traj_is_weights
-
-
+        Returns:
+            torch.Tensor: Identical tensor to traj_is_weights
+        """
+        return traj_is_weights
 
 def clip_weights(traj_is_weights:torch.Tensor, clip:float)->torch.Tensor:
     """Clips propensity weights according to the value provided in clip

tests/OPEEstimators/test_utils.py
@@ -3,10 +3,11 @@ import numpy as np
 import torch
 import unittest
 from offline_rl_ope.OPEEstimators.utils import (
-    clip_weights, clip_weights_pass, norm_weights_pass, wis_norm_weights)
+    clip_weights, clip_weights_pass, NormWeightsPass, WISNormWeights)
 from ..base import (weight_test_res, msk_test_res)
 
-
+weight_test_res_alter = copy.deepcopy(weight_test_res)
+weight_test_res_alter[0] = torch.zeros(len(weight_test_res_alter[0]))
 
 class UtilsTest(unittest.TestCase):
     
@@ -39,20 +40,45 @@ class UtilsTest(unittest.TestCase):
     def test_norm_weights_pass(self):
             test_res = copy.deepcopy(weight_test_res)
             toll = test_res.mean()/1000
-            pred_res = norm_weights_pass(traj_is_weights=weight_test_res, 
-                                         is_msk=msk_test_res)
+            calculator = NormWeightsPass()
+            pred_res = calculator(traj_is_weights=weight_test_res, 
+                                  is_msk=msk_test_res)
             self.assertEqual(pred_res.shape,weight_test_res.shape)
             np.testing.assert_allclose(pred_res.numpy(), test_res.numpy(), 
                                        atol=toll.numpy())
     
     def test_norm_weights_wis(self):
-        test_res = weight_test_res/weight_test_res.sum(axis=0)
+        denom = weight_test_res.sum(axis=0)/msk_test_res.sum(axis=0)
+        test_res = weight_test_res/denom
         toll = test_res.mean()/1000
-        pred_res = wis_norm_weights(traj_is_weights=weight_test_res, 
-                                    is_msk=msk_test_res)
+        calculator = WISNormWeights()
+        pred_res = calculator(traj_is_weights=weight_test_res, 
+                              is_msk=msk_test_res)
         self.assertEqual(pred_res.shape,weight_test_res.shape)
         np.testing.assert_allclose(pred_res.numpy(), test_res.numpy(), 
                                    atol=toll.numpy())
 
+    def test_norm_weights_wis_smooth(self):
+        smooth_eps = 0.00000001
+        denom = (weight_test_res_alter.sum(axis=0)+smooth_eps)/(
+            msk_test_res.sum(axis=0)+smooth_eps)
+        test_res: torch.Tensor = weight_test_res_alter/(denom)
+        toll = test_res.nanmean()/1000
+        calculator = WISNormWeights(smooth_eps=smooth_eps)
+        pred_res = calculator(traj_is_weights=weight_test_res_alter, 
+                              is_msk=msk_test_res)
+        self.assertEqual(pred_res.shape,weight_test_res_alter.shape)
+        np.testing.assert_allclose(pred_res.numpy(), test_res.numpy(), 
+                                   atol=toll.numpy())
         
+    def test_norm_weights_wis_no_smooth(self):
+        denom = weight_test_res_alter.sum(axis=0)/msk_test_res.sum(axis=0)
+        test_res: torch.Tensor = weight_test_res_alter/denom
+        toll = test_res.nanmean()/1000
+        calculator = WISNormWeights()
+        pred_res = calculator(traj_is_weights=weight_test_res_alter, 
+                              is_msk=msk_test_res)
+        self.assertEqual(pred_res.shape,weight_test_res_alter.shape)
+        np.testing.assert_allclose(pred_res.numpy(), test_res.numpy(), 
+                                   atol=toll.numpy(), equal_nan=True)
         