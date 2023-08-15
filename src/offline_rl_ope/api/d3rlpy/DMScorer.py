e0e60a8c0496d62d75f22bd6e5c7bddb33210f17
Joshua Spear <josh.spear9@gmail.com>
2023-08-15 08:26:25 +0100

    updated ability to directly initialise fqe model without supplying environment

--- src/offline_rl_ope/api/d3rlpy/DMScorer.py
+++ src/offline_rl_ope/api/d3rlpy/DMScorer.py
@@ -1,14 +1,17 @@
-from typing import Callable, Dict, Union
+from typing import Callable, Dict, Union, Tuple
 import shutil
 import os
 import numpy as np
 from d3rlpy.ope.fqe import FQEConfig, FQE, DiscreteFQE
-from d3rlpy.dataset import MDPDataset
+from d3rlpy.dataset import MDPDataset, Shape
 from d3rlpy.interface import QLearningAlgoProtocol
 from d3rlpy.base import DeviceArg
 from d3rlpy.logging import FileAdapterFactory
+from d3rlpy.envs import GymEnv
 
 from .utils import QueryCallbackBase
+
+FQEImplInitArg = Union[GymEnv, Tuple[Shape, int]]
 
 class FQECallback(QueryCallbackBase):
     """ Scorer class for performing Fitted Q Evaluation
@@ -16,8 +19,8 @@ class FQECallback(QueryCallbackBase):
     
     def __init__(self, scorers:Dict[str, Callable], 
                  fqe_cls:Union[FQE, DiscreteFQE], model_init_kwargs:Dict, 
-                 model_fit_kwargs:Dict, dataset:MDPDataset, env=None, 
-                 device:DeviceArg = False
+                 model_fit_kwargs:Dict, dataset:MDPDataset, 
+                 fqe_impl_init:FQEImplInitArg=None, device:DeviceArg = False
                  ) -> None:
         self.__scorers = scorers
         self.__dataset = dataset
@@ -27,14 +30,22 @@ class FQECallback(QueryCallbackBase):
         self.__logs_loc = os.path.join(os.getcwd(), "tmp_fqe_logs_loc")
         self.__cur_exp = 0
         os.mkdir(self.__logs_loc)
-        self.__env = env
+        self.__fqe_impl_init = fqe_impl_init
         self.__device = device
             
     def __call__(self, algo: QLearningAlgoProtocol, epoch:int, total_step:int):
         fqe_config = FQEConfig(**self.__model_init_kwargs)
         fqe = self.__fqe_cls(algo=algo, config=fqe_config, device=self.__device)
-        if self.__env is not None:
-            fqe.build_with_env(self.__env)
+        if self.__fqe_impl_init is not None:
+            if isinstance(self.__fqe_impl_init, Tuple):
+                if len(self.__fqe_impl_init) != 2:
+                    raise Exception
+                fqe.create_impl(
+                    observation_shape=self.__fqe_impl_init[0],
+                    action_size=self.__fqe_impl_init[1], 
+                    )
+            else:    
+                fqe.build_with_env(self.__fqe_impl_init)
         else:
             fqe.build_with_dataset(self.__dataset)
         
