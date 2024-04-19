3f7eb320ab9c6a00fded4bf70793da0698bbc13d
Joshua Spear <josh.spear9@gmail.com>
2024-04-19 19:07:46 +0100

    fixed unit test for vwp

--- tests/Metrics/test_ValidWeightsProp.py
+++ tests/Metrics/test_ValidWeightsProp.py
@@ -8,19 +8,6 @@ from ..base import weight_test_res, msk_test_res
 
 logger = logging.getLogger("offline_rl_ope")
 
-
-class TestImportanceCalc:
-    
-    def __init__(self) -> None:
-        self.weight_msk = msk_test_res
-
-class TestImportanceSampler:
-    
-    def __init__(self) -> None:
-        self.is_weight_calc = None
-        self.traj_is_weights = weight_test_res
-        self.is_weight_calc = TestImportanceCalc()
-
 class TestValidWeightsProp(unittest.TestCase):
 
     def test_call(self):
@@ -31,9 +18,8 @@ class TestValidWeightsProp(unittest.TestCase):
         denum = torch.sum(msk_test_res, axis=1)
         act_res = torch.mean(num/denum).item()
         metric = ValidWeightsProp(
-            is_obj=TestImportanceSampler(), 
             max_w=max_val,
             min_w=min_val
             )
-        pred_res = metric()
+        pred_res = metric(weights=weight_test_res, weight_msk=msk_test_res)
         self.assertEqual(act_res,pred_res)
