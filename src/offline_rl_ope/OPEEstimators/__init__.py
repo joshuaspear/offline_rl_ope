69f8a78a875c87938b5fe630189df624a1f24e85
Joshua Spear <josh.spear9@gmail.com>
2024-11-26 16:36:53 +0000

    Revert "version 8.0.0"
    This reverts commit cef21bb69ff79f37dd6b1b81ae819741de0352a2.

--- src/offline_rl_ope/OPEEstimators/__init__.py
+++ src/offline_rl_ope/OPEEstimators/__init__.py
@@ -1,5 +1,6 @@
 from .DirectMethod import DirectMethodBase, D3rlpyQlearnDM
 from .DoublyRobust import DREstimator
 from .IS import ISEstimatorBase, ISEstimator
+from .base import OPEEstimatorBase
 from .EmpiricalMeanDenom import *
 from .WeightDenom import *
